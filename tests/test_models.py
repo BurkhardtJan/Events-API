0c10f024424a803e6b85555205706303c5eaeae4
Jan Burkhardt <burkhardt.jan.august@gmail.com>
2026-05-28 16:28:34 +0200

    add CI pipeline for Events API

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,6 +6,3 @@ def test_user_password_hashing_behaves_correctly():
     assert user.check_password("mypassword123") == True
     assert user.check_password("notmypassword123") == False
     assert user.password_hash != "mypassword123"
-
-def test_false():
-    assert False
